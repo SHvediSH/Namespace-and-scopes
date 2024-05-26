654d89cb835dab7135c324cb565ce4c5897542d7
SHvediSH <74442255+SHvediSH@users.noreply.github.com>
2024-05-26 20:51:22 +1000

    Delete Homework1.py

--- Homework1.py
+++
@@ -1,8 +0,0 @@
-def test_function():
-    #inner_function()                                    #Это неправильная запись кода, невозможно вызвать функцию которая была создана после написания команды.
-    def inner_function():
-        print("Я в области видимости test_function!")
-        global inner_function
-
-test_function()
-#inner_function()                                        #Это так же не правильная запись, данная функция создана локально внутри test_function и не может быть вызвана отдельно
